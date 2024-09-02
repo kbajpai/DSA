cfa0a6e188e42b2355652e6f34be7494764a3009
Kunal Bajpai <kunal.bajpai@gmail.com>
2024-09-02 00:24:30 -0400

    Code - Initial Commit

---
+++ src/Algorithms/LeetCode/ArraysStrings.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode;
+
+public static class ArraysStrings {
+    public static int MaximumNumber(int[]? arr) {
+        if (arr == null || arr.Length == 0) {
+            throw new ArgumentException("Array cannot be null or empty");
+        }
+
+        var n = int.MinValue;
+
+        foreach (var a in arr) {
+            if (a > n) {
+                n = a;
+            }
+        }
+
+        if (n <= 0) {
+            throw new InvalidOperationException("No positive number found");
+        }
+
+        return n;
+    }
+}
---
+++ src/Algorithms/LeetCodeTests/ArraysStringsTests.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode;
+
+namespace LeetCodeTests;
+
+[TestClass]
+public class ArraysStringsTests {
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void MaximumNumber_EmptyArray_ThrowsException() {
+        int[] input = [];
+        ArraysStrings.MaximumNumber(input);
+    }
+
+    [TestMethod]
+    public void MaximumNumber_MixedNumbers_ReturnsMaximum() {
+        int[] input = [-1, -3, 5, 7, -9];
+        var result = ArraysStrings.MaximumNumber(input);
+        Assert.AreEqual(7, result);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void MaximumNumber_NoPositiveNumbers_ThrowsException() {
+        int[] input = [-1, -3, -5, -7, -9];
+        ArraysStrings.MaximumNumber(input);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void MaximumNumber_NullArray_ThrowsException() {
+        int[]? input = null;
+        ArraysStrings.MaximumNumber(input);
+    }
+
+    [TestMethod]
+    public void MaximumNumber_PositiveNumbers_ReturnsMaximum() {
+        int[] input = [1, 3, 5, 7, 9];
+        var result = ArraysStrings.MaximumNumber(input);
+        Assert.AreEqual(9, result);
+    }
+}
